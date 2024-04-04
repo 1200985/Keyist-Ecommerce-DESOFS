020782dc1ecc45718966e7bb5c6fc0ea6e7a8652
1200985 <1200985@isep.ipp.pt>
2024-04-04 17:01:44 +0100

    Add a role to the User class

--- resource_server/src/main/java/com/commerce/backend/model/entity/User.java
+++ resource_server/src/main/java/com/commerce/backend/model/entity/User.java
@@ -5,9 +5,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.Type;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
@@ -15,14 +21,14 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @ToString(exclude = "cart")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    //TODO remove this and use cart repository findByUserId instead
+    // TODO remove this and use cart repository findByUserId instead
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Cart cart;
 
@@ -62,5 +68,38 @@ public class User {
 
     @Column(name = "address")
     private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
