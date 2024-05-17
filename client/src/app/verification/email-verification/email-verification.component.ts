8e8ebe540954febc7483c841706ba9e61739ddf9
1200985 <1200985@isep.ipp.pt>
2024-05-17 14:49:26 +0100

    Remove shadowed variable

--- client/src/app/verification/email-verification/email-verification.component.ts
+++ client/src/app/verification/email-verification/email-verification.component.ts
@@ -10,33 +10,44 @@ import { catchError, take } from 'rxjs/operators';
 @Component({
   selector: 'app-email-verification',
   templateUrl: './email-verification.component.html',
-  styleUrls: ['./email-verification.component.scss']
+  styleUrls: ['./email-verification.component.scss'],
 })
 export class EmailVerificationComponent implements OnInit {
-
   isVerified: boolean = null;
   isError: boolean = null;
   isLoggedIn: boolean = null;
 
-  constructor(private route: ActivatedRoute, private accountService: AccountService, private store: Store<fromApp.AppState>) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private accountService: AccountService,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   ngOnInit() {
     const verificationToken = this.route.snapshot.queryParams.token;
-    this.accountService.verifyEmail(verificationToken).pipe(take(1), catchError(error => {
-      this.isVerified = false;
-      this.isError = true;
-      return throwError(error);
-    })).subscribe((data) => {
-      this.isVerified = true;
-      this.store.select('auth').pipe(take(1)).subscribe(data => {
-        if (data.authenticated) {
-          this.isLoggedIn = true;
-          this.store.dispatch(new AuthActions.FetchVerificationStatus());
-        } else {
-          this.isLoggedIn = false;
-        }
+    this.accountService
+      .verifyEmail(verificationToken)
+      .pipe(
+        take(1),
+        catchError((error) => {
+          this.isVerified = false;
+          this.isError = true;
+          return throwError(error);
+        })
+      )
+      .subscribe((_) => {
+        this.isVerified = true;
+        this.store
+          .select('auth')
+          .pipe(take(1))
+          .subscribe((data) => {
+            if (data.authenticated) {
+              this.isLoggedIn = true;
+              this.store.dispatch(new AuthActions.FetchVerificationStatus());
+            } else {
+              this.isLoggedIn = false;
+            }
+          });
       });
-    });
   }
 }
