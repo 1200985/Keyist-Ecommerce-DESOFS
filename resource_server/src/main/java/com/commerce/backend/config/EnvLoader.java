8c86ef16e9ed32f53de9c89b920542620950f470
João Teixeira <1201399@isep.ipp.pt>
2024-06-07 19:09:48 +0100

    Start using env

---
+++ resource_server/src/main/java/com/commerce/backend/config/EnvLoader.java
@@ -0,0 +1,10 @@
+package com.commerce.backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvLoader {
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
