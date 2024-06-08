264d056ccf07d232fc09e4c4464d67bc63887e48
João Teixeira <1201399@isep.ipp.pt>
2024-06-08 19:24:51 +0100

    Fix to running the app through docker

--- authorization_server/src/main/java/com/commerce/oauth/config/EnvLoader.java
+++ authorization_server/src/main/java/com/commerce/oauth/config/EnvLoader.java
@@ -4,7 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvLoader {
     public static void loadEnv() {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        try {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        } catch (Exception e) {
+            System.out.println("Could not load .env file, relying on system/environment variables");
+        }
     }
 }
